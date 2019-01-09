3c58524114392fac61ec34399528816b070746a4
yexufang <yexufangs@163.com>
2019-01-09 09:46:22 +0800

    create group node in admin web site dynamic

---
+++ src/main/webapp/admin/js/group_init.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  for (var i = 0; i < 10; i++){
+
+    var sortedList = document.createElement("li");
+    sortedList.className += "col-md-2 col-sm-4 col-xs-6";
+
+    var iconSpan = document.createElement("span");
+    iconSpan.className += "lnr lnr-map-marker";
+
+    var nameLink = document.createElement("a");
+    nameLink.className += "cssclass";
+    nameLink.innerHTML = "Hello" + i;
+    nameLink.setAttribute("href","http://www.baidu.com?addr=" + i);
+
+    sortedList.appendChild(iconSpan);
+    sortedList.appendChild(nameLink);
+
+    var ulist = document.getElementById("groups-ul");
+    ulist.appendChild(sortedList);
+    console.log(i);
+
+
+  }
+});
+/*
+<li class="col-md-2 col-sm-4 col-xs-6">
+  <span class="lnr lnr-map-marker"></span> <span class="cssclass">lnr lnr-map-marker</span>
+</li>
+
+var txt1="<p>Text.</p>";              // 以 HTML 创建新元素
+var txt2=$("<p></p>").text("Text.");  // 以 jQuery 创建新元素
+var txt3=document.createElement("p");
+txt3.innerHTML="Text.";               // 通过 DOM 来创建文本
+$("body").append(txt1,txt2,txt3);        // 追加新元素
+*/
